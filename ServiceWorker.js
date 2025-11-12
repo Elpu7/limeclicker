94ac8856af2b1bbae11bd5ab84864e894ce3663c
Emil <136149505+Elpu7@users.noreply.github.com>
2025-11-12 10:01:32 +0200

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,14 +1,7 @@
-const CACHE_NAME = "limeclicker-cache-v1";
-const urlsToCache = ["/", "/index.html", "/manifest.json"];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  console.log("Service Worker installed");
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  // Tämä sallii asennuksen ja perus offline-tuen
 });
