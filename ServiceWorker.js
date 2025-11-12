4976b5acbd415924b52dab567758c3ff9fffbd85
Emil <136149505+Elpu7@users.noreply.github.com>
2025-11-12 09:57:13 +0200

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,3 +1,14 @@
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker] Install');
+const CACHE_NAME = "limeclicker-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
